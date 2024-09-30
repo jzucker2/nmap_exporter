10c588805a11aee6a4b4a7097c3d8097c1d965c9
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-30 11:58:39 -0700

    Continuing basic set up here

--- app/nmap_client.py
+++ app/nmap_client.py
@@ -1,6 +1,6 @@
 import os
 from .utils import LogHelper
-from .metrics import Metrics
+# from .metrics import Metrics
 
 
 log = LogHelper.get_env_logger(__name__)
@@ -10,3 +10,8 @@ class NmapClient(object):
     @classmethod
     def get_client(cls):
         return cls()
+
+    @classmethod
+    def get_nmap_scan_host(cls):
+        return os.environ.get('NMAP_SCAN_HOST',
+                              '127.0.0.1')
